ac28fa9fed5c90996ce04a16ea262db5fba87bdf
Pangu <Pangu@DESKTOP-IF6DCCV>
2018-05-04 11:13:11 -0700

    updated domath function

--- practice29/do_math.js
+++ practice29/do_math.js
@@ -1,20 +1,39 @@
-function do_math( num1, num2, string){
+function do_math( inputNum1, inputNum2, string){
     var result;
     var operator;
+    var numbers = [];
+
+    if (isNaN(inputNum1)){
+        operator = inputNum1;
+    }else{
+        numbers.push(inputNum1);
+    }
+    if (isNaN(inputNum2)){
+        operator = inputNum2;
+    }else{
+        numbers.push(inputNum2);
+    }
+    if (isNaN(string)){
+        operator = string;
+    }else{
+        numbers.push(string);
+    }
+
+
     switch(string){
         case '+':
-            result = num1 + num2;
+            result = numbers[1] + numbers[2];
             break;
         case '-':
-            result = num1 - num2;
+            result = numbers[1] - numbers[2];
             break;
         case 'x':
         case 'X':
         case '*':
-            result = num1 * num2;
+            result = numbers[1] * numbers[2];
             break;
         case '/':
-            result = num1 / num2;
+            result = numbers[1] / numbers[2];
             break;
         default:
             result = 'wrong operator';
