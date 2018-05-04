774c4501943e0e4909c5b56f37cb7aff830f7d01
Pangu <Pangu@DESKTOP-IF6DCCV>
2018-05-04 14:58:22 -0700

    updated function to take in any amount of inputs and one operator

--- practice29/do_math.js
+++ practice29/do_math.js
@@ -1,42 +1,39 @@
-function do_math( inputNum1, inputNum2, string){
-    var result;
+function do_math(){
     var operator;
     var numbers = [];
 
-    if (isNaN(inputNum1)){
-        operator = inputNum1;
-    }else{
-        numbers.push(inputNum1);
-    }
-    if (isNaN(inputNum2)){
-        operator = inputNum2;
-    }else{
-        numbers.push(inputNum2);
-    }
-    if (isNaN(string)){
-        operator = string;
-    }else{
-        numbers.push(string);
+    function loopMath(array, operator){
+        var result = array[0];
+        for (var i = 1; i < array.length; i++){
+            switch(operator){
+                case '+':
+                    result = result + array[i]
+                    break;
+                case '-':
+                    result = result - array[i];
+                    break;
+                case 'x':
+                case 'X':
+                case '*':
+                    result = result * array[i];
+                    break;
+                case "/":
+                    result = result / array[i];
+                    break;
+                default:
+                    result = 'No operator';
+            }
+        }
+        return result;
     }
 
-
-    switch(string){
-        case '+':
-            result = numbers[1] + numbers[2];
-            break;
-        case '-':
-            result = numbers[1] - numbers[2];
-            break;
-        case 'x':
-        case 'X':
-        case '*':
-            result = numbers[1] * numbers[2];
-            break;
-        case '/':
-            result = numbers[1] / numbers[2];
-            break;
-        default:
-            result = 'wrong operator';
+    for (var i = 0; i<arguments.length; i++){
+        if (typeof arguments[i] == 'string'){
+            operator = arguments[i];
+        }else{
+            numbers.push(arguments[i]);
+        }
     }
-    return result;
+
+    return loopMath(numbers, operator);
 }
